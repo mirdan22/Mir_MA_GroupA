6ca62eb4c3eec82fbd80eaa4cc5d7d16fd44589c
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-01-30 17:46:18 -0500

    js
    added

--- script/main.js
+++ script/main.js
@@ -1,4 +1,25 @@
-(() => {
-	// set up the puzzle pieces and boards
-	
-})();
+// Vaeiables always go at the top
+// these sre the connection that you are making to elements on 
+//use CSS selection to make connections to elements  with javascript \
+// let theButton = document.querySelector("#buttonOne);
+// create a 1 to 1 connection with a varible
+let theButtons = document.querySelectorAll("#buttonHolder img"),
+	theHeading = document.querySelector("#headLine h1"),
+	puzzleboard = document.querySelector(".puzzle-board");
+
+
+// step 3
+function changeBGImage() {
+	debugger;
+	//theHeading.textContent = "Super Awesome Drag and Drop Game";
+	let theNewSource = "images/background" + this.id + "img";
+	theHeading.classList.add('orange-headline');
+	console.log(theNewSource)
+	}
+// step 2
+//1 to 1
+//theButton.addEventListener("click", changeBGimage);
+
+// 1 to many 
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
